3a538ddb730e49e193c9b1fd882ac659b0d9404d
Dharun udhaya <dharunkannan2001@gmail.com>
2023-07-19 13:33:37 +0530

    code puhed

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-console.log("hello");
+let btnContainer = document.querySelector('#buttonContainer')
+let formContainer = document.querySelectorAll('.formLabel')
+let emptyDiv = document.querySelector('#empDiv')
+let inputTitle = document.getElementById('inputBox')
+let contentBox = document.getElementById('contentLabel')
+let closePopup = document.getElementById('blurDiv')
+
+document.getElementById('sectionOuter').classList.add('hidden')
+
+let headButton = document.querySelector('.buttonLabel')
+headButton.onclick = ((event)=>{
+    event.preventDefault()
+    document.getElementById('sectionOuter').classList.remove('hidden')
+})
+
+let arr = []
+btnContainer.onclick = ((event)=>{
+
+    // emptyDiv.style.display = "block"
+    // closePopup.style.display = "block"
+
+    event.preventDefault();
+
+    let mainTitleCreate = document.createElement('p')
+    mainTitleCreate.setAttribute('class','paraLabel')
+    mainTitleCreate.innerHTML = inputTitle.value
+    emptyDiv.appendChild(mainTitleCreate)
+
+    let titleCreate = document.createElement('li')
+    titleCreate.setAttribute('class','insideList')
+    titleCreate.innerHTML = contentBox.value
+    emptyDiv.appendChild(titleCreate)
+    // console.log(titleCreate.innerHTML)
+    
+    let editOption = document.createElement('button')
+    editOption.innerHTML = "Edit"
+    editOption.setAttribute('class','editButton')
+    emptyDiv.appendChild(editOption)
+
+    let deleteOption = document.createElement('button')
+    deleteOption.innerHTML = "Delete"
+    editOption.setAttribute('class','deleteButton')
+    emptyDiv.appendChild(deleteOption)
+
+    let updateOption = document.createElement('button')
+    updateOption.innerHTML = "Update"
+    updateOption.setAttribute('class','updateButton')
+    emptyDiv.appendChild(updateOption)
+
+    function updateNode(){
+        
+    }
+
+    updateNode()
+
+
+})
+
+closePopup.onclick = (()=>{
+    emptyDiv.style.display = "none"
+    closePopup.style.display = "none"
+})
+// console.log(arr)
